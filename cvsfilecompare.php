27488aa596a26547088c3814eb0a0bc126cea3c7
Ambrose Kimaiyo <ambrose.kimaiyo@riverbank.co.ke>
2015-07-12 11:09:00 +0300

    Changes by ambrose to use OOP(Class)

--- cvsfilecompare.php
+++ cvsfilecompare.php
@@ -25,112 +25,13 @@
 <h1 class="text-center">Free Gifts Report</h1>
 
 <?php
-
-
-echo "<table class=\"table table-striped table-bordered\" id=\"thetable\">";
-echo "<tr>";
-echo "<th>Code</th> <th>Description</th> <th>Qty</th> <th>Cost</th> <th>Totals</th>";
-echo "</tr>";
-
-$files = glob("*.csv");
-usort($files, create_function('$a,$b', 'return filemtime($a) - filemtime($b);'));
-
-echo "<form action='cvsfilecompare.php' method='post'> <select name='csv'>";
-foreach($files as $filepath) {
-	if ($filepath != 'refrence.csv') {
-  echo "<option value='" . $filepath . "'>" . $filepath . "</option>";
-	}
+include "main.php";
+$main = new MainClass();
+$main->displayData();
+if ( isset($_POST['code']) ) {
+	$data = $_POST;
+	$main->addToReference($data);
 }
-echo "<input type='submit'>";
-echo "</select> </form>";
-echo "\n";
-echo "Post value: ";
-echo $_POST['csv'];
-
-$file = file( $_POST['csv'] );
-
-$file2 = file('refrence.csv');
-
-$total = array();
-$column1 = array();
-$column2 = array();
-$column3 = array();
-$test = 0;
-
-foreach ($file2 as $line2) {   //refrence file
-			list($c1,$c3)=explode(",", $line2);    // should force 2?
-			$c1 = strtolower($c1);
-			$c3 = ltrim ($c3,'$');
-			$c3 = intval($c3);
-
-			$column1[] = $c1;
-			$column3[] = $c3;
-		}
-foreach ($file as $line) {
-	list($col1,$col2,$col3,$col4,$col5)=explode(",", $line);
-	if (strpos($col3,'Free') OR strpos($col3,'FREE') !== false) {
-		
-		$col2 = strtolower($col2);
-
-		$key = array_search($col2, $column1, true);
-		if ($key == false) {
-			$price = "none";
-			$extend = "na";
-		} else {
-			$price = $column3[$key];
-			$extend = $price*$col4;
-		}
-
-			if ($price == 0) {
-				$class = "danger";
-				$test++;
-			} else {
-				$class = "";
-			}
-
-echo "<tr>";
-    	echo "<td class=\"$class\">$col2</td> <td class=\"$class\">$col3</td> <td class=\"$class\">$col4</td> <td class=\"$class\">$price</td> <td class=\"$class\">$extend</td>";
-    	array_push($total, $extend);
-echo "</tr>";
-
-	}		 
-}
-
-if ($test == 0) {
-	$class2 = "success";
-} else {
-	$class2 = "";
-}
-
-echo "<tr class=\"".$class2."\"><th colspan=\"5\" class=\"$class2\"> TOTAL: $";
-echo array_sum($total);
-echo "</th></tr>";
-echo "</table>";
-//print_r ($column1);
-//print_r ($column3);
-
-
-//  add to refrence file
-
-if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-
-	$one = $_POST["code"]; 
-	//$two = $_POST["desc"];
-	$three = $_POST["cost"];
-	//$newwrite = $one . ",\"" . $two . "\"," . $three . "\n";
-	$newline = "\n";
-	$newwrite = array($one,$three);
-	echo "<p>";
-	print_r ($newwrite);
-	echo "</p>";
-
-$fp = fopen('refrence.csv', 'a');
-fputcsv($fp, $newwrite);
-fclose($fp);
-
-}
-
-
 ?>
 </div>
 <div class"row">
@@ -164,10 +65,8 @@ Cost $<input type="text" name="cost">
     <script src="js/bootstrap.min.js"></script>
 
 <script type="text/javascript" src="table2CSV.js" >
-
 </script> 
 
 
   </body>
 </html>
-
